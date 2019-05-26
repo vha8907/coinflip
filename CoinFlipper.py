65bd6ec7e6355a76e03144810cbf7e44de8dfb94
Vinicius H. A <viniciusha@gmail.com>
2019-05-26 18:41:40 -0300

    formatting, some tests

--- CoinFlipper.py
+++ CoinFlipper.py
@@ -1,18 +1,21 @@
 import random
 
+
 def headsOrTails():
-    result = random.randint(0,1)
+    result = random.randint(0, 1)
     if result == 1:
         return "Tails"
     else:
         return "Heads"
 
+
 def flip_and_print(n):
-    for i in range(1,n+1):
-        print("Toss " + str(i)+ ": " + headsOrTails())
+    for i in range(1, n+1):
+        print("Toss " + str(i) + ": " + headsOrTails())
+
 
 def makeTable(n):
     resultTable = {}
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         resultTable[i] = headsOrTails()
     return resultTable
